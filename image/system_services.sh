e9ceed7de0fe2ddd60a6bb21b8a93cd67ebf70a4
Goran Cetusic <goran.cetusic@gmail.com>
2015-06-10 21:09:48 +0200

    added quagga and tcpdump installations

--- image/system_services.sh
+++ image/system_services.sh
@@ -67,3 +67,15 @@ cp /build/runit/cron /etc/service/cron/run
 ## Remove useless cron entries.
 # Checks for lost+found and scans for mtab.
 rm -f /etc/cron.daily/standard
+
+## Install Quagga
+$minimal_apt_get_install quagga
+if [ -d "/etc/quagga/" ]; then
+    cd /etc/quagga/
+    touch zebra.conf ripd.conf ripngd.conf ospfd.conf ospf6d.conf bgpd.conf
+    ln -s /boot.conf Quagga.conf
+fi
+
+cp /build/quaggaboot.sh /usr/local/bin
+chmod 755 /usr/local/bin/quaggaboot.sh
+ln -s /usr/lib/quagga/* /usr/local/bin
